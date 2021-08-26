134e64e3f8a747fed5eaab69dbb5265650e5a45b
myjobid101 <89570998+myjobid101@users.noreply.github.com>
2021-08-26 17:46:08 +0530

    Update Program.cs

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -7,8 +7,32 @@ namespace FileData
 {
     public static class Program
     {
-        public static void Main(string[] args)
+       public static void Main(string first,string second)
         {
-        }
+            try
+            {
+                if (first == "-v")
+                {
+                    Console.WriteLine(ThirdPartyTools.FileDetails.Version(second));
+                }
+
+                else if (first == "-v" || first == "--v" || first == "/v" || first == "--version")
+                {
+                    Console.WriteLine(ThirdPartyTools.FileDetails.Version(second));
+                }
+                else if (first == "-s" || first == "--s" || first == "/s" || first == "--size")
+                {
+                    Console.WriteLine(ThirdPartyTools.FileDetails.Size(second));
+                }
+            }
+
+
+
+            catch(Exception ex)
+            {
+                log.error(ex.Message);
+            }
+    
+    }
     }
 }
